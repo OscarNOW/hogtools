d746e7f968aa19c064f0a6ed35e3b45fd458b558
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 21:07:00 +0100

    Implemented tangent points

--- points.js
+++ points.js
@@ -1,6 +1,6 @@
 const tablePoints = {
     sine: generateSineTablePoints({ precision: 100 }),
-    tangent: [],
+    tangent: generateTangentTablePoints({ precision: 100 }),
     step: [[0.49, 1], [0.51, 0]],
     sawTooth: [[0, 0.5], [0.25, 1], [0.75, 0], [1, 0.5]],
     ramp: [[0, 0], [1, 1]],
@@ -19,6 +19,20 @@ function generateSineTablePoints({ precision }) {
     for (let i = 0; i < precision; i++) {
         let x = i / precision;
         let y = Math.sin(x * 2 * Math.PI) / 2 + 0.5;
+        points.push([x, y]);
+    }
+
+    return points;
+}
+
+function generateTangentTablePoints({ precision }) {
+    let points = [];
+
+    for (let i = 0; i < precision; i++) {
+        let x = i / precision;
+        let y = Math.tan(x * 2 * Math.PI) / 20 + 0.5;
+        y = Math.max(0, y);
+        y = Math.min(1, y);
         points.push([x, y]);
     }
 
