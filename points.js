8ea3f6a47d37cf7c0e85b9034abf8846451634e3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 23:40:42 +0100

    Fixed begin and enth

--- points.js
+++ points.js
@@ -1,7 +1,7 @@
 const tablePoints = {
     sine: generateSineTablePoints({ precision: 100 }),
     tangent: generateTangentTablePoints({ precision: 100 }),
-    step: [[0.49, 1], [0.51, -1]],
+    step: [[0, 1], [0.499, 1], [0.501, -1], [1, -1]],
     sawTooth: [[0, 0], [0.25, 1], [0.75, -1], [1, 1]],
     ramp: [[0, -1], [1, 1]],
     invRamp: [[0, 1], [1, -1]],
@@ -18,7 +18,7 @@ function generateSineTablePoints({ precision }) {
 
     for (let i = 0; i < precision; i++) {
         let x = i / (precision - 1); // -1 so there is one at the end
-        let y = Math.sin(x * 2 * Math.PI) / 2 + 0.5;
+        let y = Math.sin(x * 2 * Math.PI);
         points.push([x, y]);
     }
 
@@ -30,8 +30,8 @@ function generateTangentTablePoints({ precision }) {
 
     for (let i = 0; i < precision; i++) {
         let x = i / (precision - 1); // -1 so there is one at the end
-        let y = Math.tan(x * 2 * Math.PI) / 20 + 0.5;
-        y = Math.max(0, y);
+        let y = Math.tan(x * 2 * Math.PI) / 15;
+        y = Math.max(-1, y);
         y = Math.min(1, y);
         points.push([x, y]);
     }
@@ -57,7 +57,7 @@ export function generatePoints({
     console.warn('offset not implemented')
 
     const size = end - begin;
-    points = points.map(([x, y]) => [x, y * size + begin])
+    points = points.map(([x, y]) => [x, y * (size / 2) + ((begin + end) / 2)])
 
     console.warn('length not implemented')
     console.warn('direction not implemented')
