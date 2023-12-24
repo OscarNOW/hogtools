7ba55d1543f16360a9328132a77b5ac3804c1d34
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 20:40:29 +0100

    Created simple renderer

--- render.js
+++ render.js
@@ -1,3 +1,15 @@
-export function render(points) {
+const canvas = document.getElementById('render');
+const ctx = canvas.getContext("2d");
 
+const xScale = 500;
+const yScale = 500;
+
+export function render(points) {
+    ctx.beginPath();
+    ctx.moveTo(points[0][0], points[0][1]);
+
+    for (const [x, y] of points)
+        ctx.lineTo(x * xScale, y * yScale);
+
+    ctx.stroke();
 }
