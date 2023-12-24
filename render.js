8e9ee5ecd7cbb187268985dfbe19b8920d34b08e
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 21:09:37 +0100

    Fixed bug in renderer

--- render.js
+++ render.js
@@ -6,8 +6,8 @@ const yScale = 500;
 
 export function render(points) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
     ctx.moveTo(points[0][0], points[0][1]);
+    ctx.beginPath();
 
     for (const [x, y] of points)
         ctx.lineTo(x * xScale, (1 - y) * yScale);
