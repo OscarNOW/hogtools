3c51a2847bec31fce9e0e249ecee98cad618c8b1
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 16:46:28 +0100

    Fixed bugs with time validation

--- validations/time.js
+++ validations/time.js
@@ -1,33 +1,34 @@
-const typeCharacters = 'smh'.split('');
+const typeCharacters = [['s', 1], ['m', 60], ['h', 3600]];
 
 export function time(inp) {
     let input = inp;
     input = input.replaceAll('*', ' ');
 
-    for (const typeCharacter of typeCharacters)
+    for (const [typeCharacter] of typeCharacters)
         input = input.replaceAll(typeCharacter, `${typeCharacter}*`);
 
     input = input.split('*');
-    input.pop();
+    if (input[input.length - 1].trim() !== '') input[input.length - 1] = input[input.length - 1] + 's';
     input = input.filter(x => x.length > 0);
     input = input.map(x => [x.substring(0, x.length - 1), x[x.length - 1]]);
-    input = input.map(([amount, type]) => ([parseInt(amount), type]));
+    if (input.length === 0) input.push(['0', 's']);
+    input = input.map(([amount, type]) => ([parseFloat(amount), type]));
     input = input.map(([amount, type]) => ([isNaN(amount) ? 0 : amount, type]));
 
     let typesAmount = [];
 
     for (const [amount, type] of input) {
-        const index = typeCharacters.indexOf(type);
+        const index = typeCharacters.findIndex(([foundType]) => foundType === type);
         if (index + 1 > typesAmount.length)
             typesAmount = [...typesAmount, ...Array(index + 1 - typesAmount.length).fill(0)];
 
-        typesAmount[index] += parseInt(amount);
+        typesAmount[index] += parseFloat(amount);
     }
 
     let output = '';
     for (let index = typesAmount.length - 1; index >= 0; index--) {
         const typeAmount = typesAmount[index];
-        output += `${typeAmount}${typeCharacters[index]}${index == 0 ? '' : ' '}`;
+        output += `${typeAmount}${typeCharacters[index][0]}${index == 0 ? '' : ' '}`;
     }
 
     return output;
