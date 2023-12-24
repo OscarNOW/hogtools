3dbe231cc5211979ce9a0535aa4d7d02106e59e5
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-12-24 17:30:37 +0100

    Fixed bug with time encoding

--- validations/time.js
+++ validations/time.js
@@ -15,19 +15,36 @@ export function time(inp) {
     input = input.map(([amount, type]) => ([parseFloat(amount), type]));
     input = input.map(([amount, type]) => ([isNaN(amount) ? 0 : amount, type]));
 
-    let typesAmount = [];
-
+    let secondAmount = 0;
     for (const [amount, type] of input) {
         const index = typeCharacters.findIndex(([foundType]) => foundType === type);
-        if (index + 1 > typesAmount.length)
-            typesAmount = [...typesAmount, ...Array(index + 1 - typesAmount.length).fill(0)];
-
-        typesAmount[index] += parseFloat(amount);
+        secondAmount += amount * typeCharacters[index][1];
     }
 
+    let typeAmounts = [];
+
+    for (const [type, typeSecondAmount] of [...typeCharacters].reverse()) {
+        let typeAmount = 0;
+
+        typeAmount = Math.floor(secondAmount / typeSecondAmount);
+        secondAmount -= typeAmount * typeSecondAmount;
+
+        typeAmounts.push(typeAmount);
+    }
+
+    typeAmounts = typeAmounts.reverse();
+
+    for (let index = typeAmounts.length - 1; index >= 0; index--) {
+        if (typeAmounts[index] === 0 && index !== 0)
+            typeAmounts[index] = null;
+        else
+            break;
+    }
+    typeAmounts = typeAmounts.filter(x => x !== null);
+
     let output = '';
-    for (let index = typesAmount.length - 1; index >= 0; index--) {
-        const typeAmount = typesAmount[index];
+    for (let index = typeAmounts.length - 1; index >= 0; index--) {
+        const typeAmount = typeAmounts[index];
         output += `${typeAmount}${typeCharacters[index][0]}${index == 0 ? '' : ' '}`;
     }
 
